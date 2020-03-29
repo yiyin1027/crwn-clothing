0a68d8e16ee3791414f071415352a23dd49f5e85
--replace-all <yzhan647@uottawa.ca>
2020-03-29 18:02:35 -0400

    Updates

---
+++ precache-manifest.1e10fef21421e4ba0ac1392c639aef1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a23abf9d810e6ff5985017869eb612",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "b40e0eb50a4e6012fd9f",
+    "url": "/crwn-clothing/static/css/main.b99e2d3c.chunk.css"
+  },
+  {
+    "revision": "723b1ded19f9221279c1",
+    "url": "/crwn-clothing/static/js/2.70968740.chunk.js"
+  },
+  {
+    "revision": "9cc59aa88bbb965336535eb6b360773d",
+    "url": "/crwn-clothing/static/js/2.70968740.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b40e0eb50a4e6012fd9f",
+    "url": "/crwn-clothing/static/js/main.b0d8a824.chunk.js"
+  },
+  {
+    "revision": "30440257c9af28ec15a5",
+    "url": "/crwn-clothing/static/js/runtime-main.b0505bf0.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-clothing/static/media/crown.ed28accb.svg"
+  }
+]);
