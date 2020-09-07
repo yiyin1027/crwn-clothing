bbf5be66114cc8abeda4422ceeaebd6d365265c5
Yiyin Zhang <yzhan647@uottawa.ca>
2020-09-07 16:55:30 -0400

    fix store.js export

--- src/redux/store.js
+++ src/redux/store.js
@@ -11,5 +11,3 @@ const middlewares = [logger];
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store)
-
-export default { store, persistStore };
