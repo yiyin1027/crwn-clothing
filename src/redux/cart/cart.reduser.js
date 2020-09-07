b7f9e31c0029ab4f41895b7cc7890f5f0b3465dc
Yiyin Zhang <yzhan647@uottawa.ca>
2020-09-07 15:44:06 -0400

    added addItem and removeItem funtionality to checkout item component

--- src/redux/cart/cart.reduser.js
+++ src/redux/cart/cart.reduser.js
@@ -1,5 +1,5 @@
 import CartActionTypes from './cart.types';
-import {addItemToCart} from './cart.utils';
+import {addItemToCart, removeItemFromCart} from './cart.utils';
 
 const INITIAL_STATE = {
   hidden: true,
@@ -16,7 +16,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case CartActionTypes.ADD_ITEM:
       return {
         ...state,
-        cartItems: addItemToCart(state.cartItems, action.payload)//[...state.cartItem, action.payload]
+        cartItems: addItemToCart(state.cartItems, action.payload)
       };
     case CartActionTypes.CLEAR_ITEM_FROM_CART:
       return {
@@ -24,6 +24,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         cartItems: state.cartItems.filter(
           cartItem => cartItem.id !== action.payload.id
         )
+      };
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload)
       };
     default:
       return state;
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,15 +1,35 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
   const existingCartItem = cartItems.find(
-    cartItem => cartItem.id === cartItemToAdd.id
+    (cartItem) => cartItem.id === cartItemToAdd.id
   );
 
-  if(existingCartItem) {
-    console.log("exist")
-    return cartItems.map(cartItem => 
-      cartItem.id === cartItemToAdd.id ? {...cartItem, quantity: cartItem.quantity + 1}  : cartItem
-    )
+  if (existingCartItem) {
+    console.log("exist");
+    return cartItems.map((cartItem) =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
   }
-  console.log("not exist")
-  
-  return [...cartItems, {...cartItemToAdd, quantity: 1}];
+  console.log("not exist");
+
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  );
+
+  if (existingCartItem.quantity === 1) {
+    console.log("item == 1")
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+
+  }
+
+  return cartItems.map(cartItem =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  )
+}
