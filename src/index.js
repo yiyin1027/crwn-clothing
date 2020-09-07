fe531fd43d4034df4f4dbf55661ed4162b405b91
Yiyin Zhang <yzhan647@uottawa.ca>
2020-09-07 16:53:54 -0400

    added redux-persist to persist our cart reducer across sessions

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-import {BrowserRouter} from 'react-router-dom';
-import {Provider} from 'react-redux';
-import store from './redux/store';
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
 
 ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <PersistGate persistor={persistor}>
         <App />
-        </BrowserRouter>
-    </Provider>,
-     document.getElementById('root')
+      </PersistGate>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,15 @@
 import {createStore, applyMiddleware} from 'redux';
 
+import { persistStore } from 'redux-persist';
+
 import {logger} from 'redux-logger';
 
 import rootReducer from './root.reduser';
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store)
+
+export default { store, persistStore };
