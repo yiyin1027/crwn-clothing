722ff76d4d1b037ac48ffb3970945986441ce760
Yiyin Zhang <yzhan647@uottawa.ca>
2020-09-06 20:49:22 -0400

    added clearItemFromCart action and bound it to remove in checkout-item

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,6 +1,7 @@
 const CartActionTypes = {
   TOGGLE_CARD_HIDDEN: 'TOGGLE_CARD_HIDDEN',
-  ADD_ITEM: 'ADD_ITEM'
+  ADD_ITEM: 'ADD_ITEM',
+  CLEAR_ITEM_FROM_CART: 'CLEAR_ITEM_FROM_CART'
 };
 
 export default CartActionTypes;
